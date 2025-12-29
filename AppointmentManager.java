f3d3bdf4fdcc5a15e6b13e62d10701635bcaa35e
EddieN03 <eddiefalcon20@gmail.com>
2025-12-29 14:32:43 -0600

    Update AppointmentManager.java
    Edgecases + pseudocode for requirement 4 are here

--- AppointmentManager.java
+++ AppointmentManager.java
@@ -43,5 +43,50 @@ public class AppointmentManager {
         //Have to convert to a stream in order to use the filter where endTime is after the current time
         return eventsEachDay.get(today).stream().filter(e -> e.getEndTime().isAfter(now)).toList();
     }
+
+    /*
+     * 4) Provide the next available slot of a specified size for today OR the specified day.
+     */
+    public Optional<LocalTime[]> findNextAvailableSlot(LocalDate aDay, Duration howLong) {
+
+        //Variables for the function
+        List<Event> eventsToCheck;
+        boolean isToday = false;
+        //Predefine If there are no events & not today, just suggest midnight + duration (as this is the first possible time)
+        LocalTime[] result = {LocalTime.MIDNIGHT, LocalTime.MIDNIGHT.plus(howLong)};
+
+        //Cut the events to be only what is currently possible
+        if (aDay.isEqual(LocalDate.now())) {
+            eventsToCheck= listTodaysRemainingEvents();
+            isToday = true;
+        } else {
+            eventsToCheck = listADaysEvents(aDay);
+        }
+
+        
+        if (eventsToCheck.isEmpty()) {
+            //This could be an &&, but I also wanted to make it so that we avoid the main algo if a future day is empty
+            if (isToday) {
+                result[0] = LocalTime.now();
+                result[1] = LocalTime.now().plus(howLong);
+            }
+            return Optional.of(result);
+        }
+
+        /*
+        * For each remaining event:
+        *   if there does NOT exist an event where (the event's endtime + howLong) does NOT intersect the next event 
+        *       return that time slot
+        *
+        * if nothing has been returned, return empty set
+        */
+
+        for (Event event : eventsToCheck) {
+            //TODO: finish the logic in here
+        }
+
+        
+        return Optional.empty();
+    }
     
 }
