dc5854e985a811316bfa172b08ec72a5bdcb07a8
EddieN03 <eddiefalcon20@gmail.com>
2025-12-29 12:22:18 -0600

    Groundwork for the Project
    Set up for the files I think will be needed for the project along with some setup for how Events will be made in this application

---
+++ Event.java
@@ -0,0 +1,43 @@
+/*
+ * An event has:
+ * String title;
+ * LocalTime starttime;
+ * LocalTime endtime;
+ * Events should NOT overlap
+ */
+
+import java.time.LocalTime;
+
+public class Event {
+
+    private final String title;
+    private final LocalTime startTime;
+    private final LocalTime endTime;
+
+    //Constructor
+    public Event(String title, LocalTime starTime, LocalTime endTime) {
+        
+        //Error handling
+        if (starTime.isAfter(endTime)) {
+            throw new IllegalArgumentException("The starting time must be before the ending time.");
+        }
+
+        this.title = title;
+        this.startTime = starTime;
+        this.endTime = endTime;
+    }
+
+    //Getters
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+    
+}
---
+++ SimpleCalendarApp.java
@@ -0,0 +1,28 @@
+/*
+ * Simple Calendar Application
+ * 
+ * This application should support:
+ * 1) Listing all events for the day
+ * 2) List all remaining events for the day
+ * 3) List all events for any specified day
+ * 4) Porvide the next available slot of a specified size for today OR the specified day.
+ * 
+ */
+
+  /*
+   * Initial Thoughts:
+   * Dictionary of days with value being the events of the day
+   * When adding an event
+   */
+
+public class SimpleCalendarApp {
+
+    public static void main(String[] args) {
+
+
+
+    }
+    
+
+    
+}
